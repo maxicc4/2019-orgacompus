f6a4257156317c48bb796e5e369c67157d92405e
Maxi Burastero <maxicc4@gmail.com>
2019-09-16 22:14:24 -0300

    Se agregan tests

--- tests.sh
+++ tests.sh
@@ -14,8 +14,37 @@ prueba1()
 	rm $BASEDIR/tests/temp.txt
 }
 
+prueba2()
+{
+	cat $BASEDIR/tests/input2.txt | ./tp0 > $BASEDIR/tests/temp.txt
+
+	diff -q $BASEDIR/tests/temp.txt $BASEDIR/tests/output2.txt 1>/dev/null
+	if [ $? -eq 0 ]; then
+		RESULTADO="OK"
+	else
+		RESULTADO="ERROR"
+	fi
+	rm $BASEDIR/tests/temp.txt
+}
+
+prueba3()
+{
+	cat $BASEDIR/tests/input3.txt | ./tp0 > $BASEDIR/tests/temp.txt
+
+	diff -q $BASEDIR/tests/temp.txt $BASEDIR/tests/output3.txt 1>/dev/null
+	if [ $? -eq 0 ]; then
+		RESULTADO="OK"
+	else
+		RESULTADO="ERROR"
+	fi
+	rm $BASEDIR/tests/temp.txt
+}
+
 echo "Ejecutando pruebas"
 
 prueba1
 echo "Prueba 1: $RESULTADO"
-
+prueba2
+echo "Prueba 2: $RESULTADO"
+prueba3
+echo "Prueba 3: $RESULTADO"
