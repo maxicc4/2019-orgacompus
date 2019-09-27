59a96d4b593b032d7e063a9d970ff7006fe8627d
Maxi Burastero <maxicc4@gmail.com>
2019-09-26 21:33:28 -0300

    Se validan los malloc al crear la matriz

--- main.c
+++ main.c
@@ -186,9 +186,17 @@ static void do_version(const char *name)
 matrix_t* create_matrix(size_t rows, size_t cols)
 {
 	matrix_t *matrix = malloc(sizeof(matrix_t));
+	if (matrix == NULL) {
+		fprintf(stderr, "Error: cannot allocate memory.\n");
+		exit(1);
+	}
 	matrix->rows = rows;
 	matrix->cols = cols;
 	matrix->array = malloc(rows*cols*sizeof(double));
+	if (matrix->array == NULL) {
+		fprintf(stderr, "Error: cannot allocate memory.\n");
+		exit(1);
+	}
 
 	return matrix;
 }
